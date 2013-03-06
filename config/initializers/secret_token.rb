05888db33e470886227f41bdb65bda54da70e3ef
Anders Ramsay <andersr@gmail.com>
2013-03-06 13:58:07 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SuggestionBoxApp::Application.config.secret_token = 'caac80cc4c19591d1201a0293e4eed3130eb4dc31e562c652c74ebf4f54cdd89e02b6722ebd3b2eb6c59de270d283ab61ef6a5dcde439108627348e6c065796e'
