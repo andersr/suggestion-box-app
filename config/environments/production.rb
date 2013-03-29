4d8fca629160dd35fa6114a20c0833c23a2c14b3
Anders Ramsay <andersr@gmail.com>
2013-03-29 14:04:38 -0400

    config.assets.compile = true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 SuggestionBoxApp::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
-  Sass::Plugin.options[:never_update] = true
+  #Sass::Plugin.options[:never_update] = true
 
   # Code is not reloaded between requests
   config.cache_classes = true
@@ -16,7 +16,7 @@ SuggestionBoxApp::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
