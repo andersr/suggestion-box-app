305f8a38112396d1223f007cab569b4b475eaf45
Anders Ramsay <andersr@gmail.com>
2013-07-26 11:23:44 -0400

    actsastextcaptcha yml, misc css tweaks

---
+++ app/assets/javascripts/suggestions.js
@@ -0,0 +1,28 @@
+# Place all the behaviors and hooks related to the matching controller here.
+# All this logic will automatically be available in application.js.
+# You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
+
+
+$('input, textarea').each(function() {
+ 
+    var self = $(this),
+        label = $("label[for='" + self.attr("id") + "']").text();
+        
+    // Replace initial values
+    if (!self.val().replace(/^\s+/g, "").length) {
+        self.val(label).addClass("placeholder"); 
+    }
+        
+    self.on("focus", function() {
+        if (self.val() === label) {
+            self.val('').removeClass("placeholder");
+        }
+    });
+        
+    self.on("blur", function() {
+        if (!self.val().replace(/^\s+/g, "")) {
+            self.val(label).addClass("placeholder");
+        }
+    });
+ 
+});
