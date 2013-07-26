5ba1347fa5c373bfc45338bce3472fd9cfd648d9
Anders Ramsay <andersr@gmail.com>
2013-07-26 11:52:16 -0400

    media query for viewport wider than 30em tho issue remains w tablet landscape viewport being too wide

--- app/models/suggestion.rb
+++ app/models/suggestion.rb
@@ -13,11 +13,16 @@
 #  email             :string(255)
 #
 
-    class Suggestion < ActiveRecord::Base
-      attr_accessible :message, :anonymous, :name, :email, :spam_answer
-      belongs_to :suggestion_box
-      validates :message, presence: true
-      acts_as_textcaptcha :api_key => 'azbjl8lm6u0cg4kos80o44cssdem847a', :bcrypt_salt => '$2a$10$UGMpZISBfrKcnRgatlu5P.'
+class Suggestion < ActiveRecord::Base
+  attr_accessible :message, :anonymous, :name, :email, :spam_answer
+  belongs_to :suggestion_box
+  validates :message, presence: true
 
-      #validates :suggestion_message, :presence => {:message => "Please enter a suggestion."}
-    end
+  acts_as_textcaptcha 	:api_key => 'azbjl8lm6u0cg4kos80o44cssdem847a',
+  						:bcrypt_salt => '$2a$10$UGMpZISBfrKcnRgatlu5P.',
+  						:bcrypt_cost => '3',
+  						:questions   => [{ 'question' => '1+1', 'answers' => '2,two' },
+                                       { 'question' => 'The green hat is what color?', 'answers' => 'green' }]
+ 
+  #validates :suggestion_message, :presence => {:message => "Please enter a suggestion."}
+end
